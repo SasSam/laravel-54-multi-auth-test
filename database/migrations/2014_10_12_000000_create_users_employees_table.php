01fcf7b28493f05e6b186e211ee884c1a9f0a53b
Gabor Farkas <sas.mester@gmail.com>
2017-02-03 18:52:53 +0000

    some bugfixes

--- database/migrations/2014_10_12_000000_create_users_employees_table.php
+++ database/migrations/2014_10_12_000000_create_users_employees_table.php
@@ -4,32 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersEmployeesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users_employees', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+class CreateUsersEmployeesTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('users_employees', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->tinyInteger('avail')->default(1);
+			$table->rememberToken();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users_employees');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('users_employees');
+	}
 }
